6764f98b36e321a134ade032d50f2c7b64d791c7
noah reinagel <noah.r9@gmail.com>
2017-02-07 22:09:34 -0700

    Made it so I can dynamically load contents

--- src/magiccardserver/WebsiteServer.java
+++ src/magiccardserver/WebsiteServer.java
@@ -1,6 +1,15 @@
 package magiccardserver;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class WebsiteServer {
+    public final Map<String, String> mimeTypes = new HashMap<String, String>() {{
+       put("html", "text/html");
+       put("js", "text/javascript");
+       put("css", "text/css");
+       put("ico", "image/x-icon");
+    }};
     private final HttpServer server;
 
     public WebsiteServer() {
@@ -15,9 +24,19 @@ public class WebsiteServer {
     private NanoHTTPD.Response serve(NanoHTTPD.IHTTPSession session) {
         String uri = session.getUri();
         NanoHTTPD.Method method = session.getMethod();
+
         if (method != NanoHTTPD.Method.GET)
             return createHttpResponse(NanoHTTPD.Response.Status.NOT_FOUND, "text/html", new DefaultResourceFactory().getString("../site/Error.html"));
-        return createHttpResponse(NanoHTTPD.Response.Status.NOT_FOUND, "text/html", new DefaultResourceFactory().getString("../site/Error.html"));
+
+        System.out.println(uri);
+
+        if(uri.equals("/"))
+            return createHttpResponse(NanoHTTPD.Response.Status.OK, "text/html", new DefaultResourceFactory().getString("../site/Main.html"));
+
+        String[] uriParts = uri.split("\\.");
+        String extension = uriParts[uriParts.length - 1];
+        String mimeType = mimeTypes.get(extension);
+        return createHttpResponse(NanoHTTPD.Response.Status.OK, mimeType, new DefaultResourceFactory().getString("../site/" + uri));
     }
 
     private NanoHTTPD.Response createHttpResponse(final NanoHTTPD.Response.Status httpStatus, String contentType, String body)
